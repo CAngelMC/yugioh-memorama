51a4ff4f57b292ebc236c097afecc2f47c96e3f9
Angel <angel_cast@outlook.es>
2021-01-24 23:59:15 -0600

    Se incrementaron los valores que necesitamos de nuestra baraja (habrá que revisar eso al momento de hacer el request a la api), y en la funcion handleMatch tendremos la logica del juego

--- src/components/BoardGame.jsx
+++ src/components/BoardGame.jsx
@@ -1,36 +1,70 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
-import Card from './Card';
-import '../assets/styles/BoardGame.scss';
+import React, { useState } from "react";
+import Card from "./Card";
+import "../assets/styles/BoardGame.scss";
 
-const cards = [
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
+const queryCards = [
+  { id: "1", value: "1", status: false },
+  { id: "2", value: "2", status: false },
+  { id: "3", value: "3", status: false },
+  { id: "4", value: "4", status: false },
+  { id: "5", value: "5", status: false },
+  { id: "6", value: "6", status: false },
+  { id: "7", value: "7", status: false },
+  { id: "8", value: "8", status: false },
+  { id: "9", value: "9", status: false },
+  { id: "10", value: "1", status: false },
+  { id: "11", value: "2", status: false },
+  { id: "12", value: "3", status: false },
+  { id: "13", value: "4", status: false },
+  { id: "14", value: "5", status: false },
+  { id: "15", value: "6", status: false },
+  { id: "16", value: "7", status: false },
+  { id: "17", value: "8", status: false },
+  { id: "18", value: "9", status: false },
 ];
 
 const BoardGame = () => {
+  const [firstCard, setFirstCard] = useState("");
+  const [secondCard, setSecondCard] = useState("");
+  const [cards, setCards] = useState(queryCards);
+
+  const handleMatch = ({id, value}) => {
+    const newCards = cards.map((card) => {
+      const newCard = {
+        ...card,
+        status: card.id === id || card.status,
+      };
+      return newCard;
+    });
+    console.log("newCards", newCards);
+    setCards(newCards);
+    if (firstCard === "") {
+      setFirstCard(value);
+    } else if (secondCard === "") {
+      setSecondCard(value);
+      if (firstCard === value) {
+        console.log("Its a match!");
+      }
+    } else {
+      setFirstCard("");
+      setSecondCard("");
+    }
+  };
+
   return (
     <div>
-      <div className='boardGame'>
+      <div className="boardGame">
         {cards.map((card, i) => {
           return (
-            <Card key={i} className='card' />
+            // <Card key={i} className='card' />
+            <Card
+              key={i}
+              card={card}
+              firstCard={firstCard}
+              secondCard={secondCard}
+              handleMatch={handleMatch}
+            />
           );
         })}
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
-import reverseCard from '../assets/static/card.png';
-import blankCard from '../assets/static/blank.jpg';
-import '../assets/styles/BoardGame.scss';
+import React, { useState } from "react";
+import reverseCard from "../assets/static/card.png";
+import blankCard from "../assets/static/blank.jpg";
+import "../assets/styles/BoardGame.scss";
 
-const Card = () => {
+const Card = (props) => {
   const [flip, setFlip] = useState(false);
 
   const handleClick = (e) => {
-    setFlip(!flip);
-    console.log(flip);
+    if (!props.card.status) {
+      setFlip(!flip);
+      props.handleMatch(props.card);
+    }
   };
 
   return (
     <button
-      type='button'
-      className={`card${flip ? ' flipped' : ''}`}
+      type="button"
+      className={`card${flip ? " flipped" : ""}`}
       onClick={handleClick}
     >
       {flip ? (
-        <img className='front' src={blankCard} alt='card' />
+        <img className="front" src={blankCard} alt="card" />
       ) : (
-        <img className='back' src={reverseCard} alt='card' />
+        <img className="back" src={reverseCard} alt="card" />
       )}
     </button>
   );
